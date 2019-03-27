eebcc18c05134a623d97ffd3138f683f3652bcb6
Brandon G-Home <brandon.gardner2@outlook.com>
2019-03-27 16:12:00 -0500

    All CRUD operations working and a join

--- routes/students-route.js
+++ routes/students-route.js
@@ -1,4 +1,15 @@
 const router = require("express").Router();
+const knex = require("knex");
+
+const knexConfig = {
+  client: "sqlite3",
+  connection: {
+    filename: "./data/lambda.sqlite3"
+  },
+  useNullAsDefault: true
+};
+
+const db = knex(knexConfig);
 
 router.route("/");
 
--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 const express = require("express");
 const helmet = require("helmet");
-const knex = require("knex");
 
 const cohortsRouter = require("./routes/cohorts-route.js");
 const studentsRouter = require("./routes/students-route.js");
 
-const knexConfig = {
-  client: "sqlite3",
-  connection: {
-    filename: "./data/lambda.sqlite3"
-  },
-  useNullAsDefault: true
-};
-
-//Server and Database initialization
-const db = knex(knexConfig);
 const server = express();
 
 //Middleware
